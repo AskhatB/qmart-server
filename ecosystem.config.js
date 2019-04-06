163a19fc0f3c3b635d9ef50b0ce5ea6c149515f1
Askhat <baltabaev.a2509@gmail.com>
2019-04-06 18:59:10 +0600

    ecosystem added

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'qmart-server',
+      script    : 'npm start',
+      args      : 'run start:production',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+  deploy : {
+    production : {},
+    staging: {
+      user: 'your-user',
+      host: 'your-server',
+      ref: 'origin/master',
+      repo: 'git@github.com:gituser/yourrepo.git',
+      path: '/var/www/yourprojectpath',
+      key: '/absolute/path/to/key',
+      ssh_options: ['ForwardAgent=yes'],
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {}
+  }
+};
